6b1472330a50a0f9e4c05f659ca135517b96de7c
RostamKhani <RostamKhani.Hamed@TaraTechGroup.com>
2020-11-10 08:58:26 +0330

    1399-08-20

--- src/app/check-mobile/check-mobile.component.ts
+++ src/app/check-mobile/check-mobile.component.ts
@@ -14,12 +14,16 @@ export class CheckMobileComponent implements OnInit {
   param = [];
   result;
   refNo;
+  shopCashDeskCount = 0;
   constructor(private httpclient: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
     this.refNo = localStorage.getItem('refNo');
     this.rc = new RestCaller(this.httpclient);
   }
+  //=============================================================================================
+  //******************************************************************************************* */
+  //=============================================================================================
   showPersoneDetail(path: string, mobileNo: string) {
     var apiResultForPersoneData;
     var json = { mobile: mobileNo };
@@ -28,15 +32,6 @@ export class CheckMobileComponent implements OnInit {
       this.showResultForMemberInfo(element);
     });
   }
-  goForUnblockingMember(path: string, tmid) {
-    var apiResultForUnblocking;
-    var json = { tara_member_id: tmid };
-    apiResultForUnblocking = this.rc.callApi('POST', path, json);
-    apiResultForUnblocking.forEach(element => {
-      this.showResultForUnblocking(element);
-    });
-  }
-  // ============================================================================================
   showResultForMemberInfo(elemnt) {
 
     var arrayLenght = elemnt.length;
@@ -53,10 +48,22 @@ export class CheckMobileComponent implements OnInit {
     }
 
   }
+  //=============================================================================================
+  //******************************************************************************************* */
+  //=============================================================================================
+  goForUnblockingMember(path: string, tmid) {
+    var apiResultForUnblocking;
+    var json = { tara_member_id: tmid };
+    apiResultForUnblocking = this.rc.callApi('POST', path, json);
+    apiResultForUnblocking.forEach(element => {
+      this.showResultForUnblocking(element);
+    });
+  }
   showResultForUnblocking(elemnt) {
     var param = [];
     var result;
     var arrayLenght = elemnt.length;
+
     for (var i = 0; i < arrayLenght; i++) {
       param[i] = elemnt[i];
     }
@@ -71,6 +78,98 @@ export class CheckMobileComponent implements OnInit {
 
   }
 
+  //=============================================================================================
+  //******************************************************************************************* */
+  //=============================================================================================
+  goForMemberPassSend(path: string, mobile) {
+    var apiResultForUnblocking;
+    var json = { mobile_number: mobile };
+    apiResultForUnblocking = this.rc.callApi('POST', path, json);
+    apiResultForUnblocking.forEach(element => {
+      this.showResultForMemberPassSend(element);
+    });
+  }
+  showResultForMemberPassSend(elemnt) {
+    var param = [];
+    var result;
+    var arrayLenght = elemnt.length;
+    for (var i = 0; i < arrayLenght; i++) {
+      param[i] = elemnt[i];
+    }
+    result = Number(param[0].MESSAGE);
+
+    if (result == 1) {
+      alert('با موفقیت انجام شد');
+    }
+    else {
+      alert('بروز مشکل در ارسال رمز کاربر');
+    }
+  }
+  //=============================================================================================
+  //******************************************************************************************* */
+  //=============================================================================================
+  showShopCash(path: string, mobileNo: string) {
+    var apiResultForPersoneData;
+    var json = { mobile: mobileNo };
+    apiResultForPersoneData = this.rc.callApi('POST', path, json);
+    apiResultForPersoneData.forEach(element => {
+      this.showResultForcashInfo(element);
+    });
+  }
+  showResultForcashInfo(elemnt) {
+    var arrayLenght = elemnt.length;
+    for (var i = 0; i < arrayLenght; i++) {
+      this.param[i] = elemnt[i];
+    }
+    var msg;
+    try{
+      msg = elemnt[0].MSG.trim();
+    }catch(e){}
+     
+    if (arrayLenght > 0 && msg != "EXECUTE SUCCESS") {
+      this.shopCashDeskCount = arrayLenght;
+      // this.router.navigateByUrl('/GetMemberData', { skipLocationChange: true });
+    }
+    else {
+      alert('شماره وارد شده، اشتباه است');
+    }
+
+  }
+  //=============================================================================================
+  //******************************************************************************************* */
+  //=============================================================================================
+  getShopInfo(path: string, casgDeskNo) {
+    var apiResultForPersoneData;
+    var json = { cash_desk_no: casgDeskNo };
+    apiResultForPersoneData = this.rc.callApi('POST', path, json);
+    apiResultForPersoneData.forEach(element => {
+      this.showForShopInfoByCashDeskNo(element);
+    });
+  }
+  showForShopInfoByCashDeskNo(elemnt){
+    var arrayLenght = elemnt.length;
+    for (var i = 0; i < arrayLenght; i++) {
+      this.param[i] = elemnt[i];
+    }
+    var msg;
+    try{
+      msg = elemnt[0].MSG.trim();
+    }catch(e){}
+     
+    if (arrayLenght > 0 && msg != "EXECUTE SUCCESS") {
+      alert(this.param[0].NAME);
+      // this.router.navigateByUrl('/GetMemberData', { skipLocationChange: true });
+    }
+    else {
+      alert('بروز مشکل در گرفتن داده ها از سروز');
+    }
+  }
+  //=============================================================================================
+  //******************************************************************************************* */
+  //=============================================================================================
+  goForShopPassSend(path: string, casgDeskNo){
+
+  }
 }
 
 
